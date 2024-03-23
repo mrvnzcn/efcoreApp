a538c70fa4cdad71fcae8e392ccb2f03f28f36dc
mrvnzcn <imervanozcan@gmail.com>
2024-03-23 19:42:00 +0300

    kurs öğrencilerinin listelenmesi

--- Controllers/KursController.cs
+++ Controllers/KursController.cs
@@ -41,7 +41,11 @@ namespace efcoreApp.Controllers
                 return NotFound(); 
             }
 
-            var kurs = await _context.Kurslar.FindAsync(id);
+            var kurs = await _context
+                            .Kurslar
+                            .Include(k => k.KursKayitlari)
+                            .ThenInclude(k => k.Ogrenci)
+                            .FirstOrDefaultAsync(k => k.KursId == id);
             // var ogr = await _context.Ogrenciler.FirstOrDefaultAsync(o => o.OgrenciId == id);
 
             if(kurs == null)
